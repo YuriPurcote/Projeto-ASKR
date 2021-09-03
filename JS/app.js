a4702ed64e5966a453f5e00e7a70fc29cca7aaec
YuriPurcote <87339208+YuriPurcote@users.noreply.github.com>
2021-09-03 19:05:43 -0300

    add

--- JS/app.js
+++ JS/app.js
@@ -15,48 +15,46 @@ function scrollToIdOnClick(event) {
         behavior: "smooth",
     });
 }
-    /* para a musica */
-    var audio =document.getElementById('audio');
-    var playMusicBTN =document.getElementById('pause');
-    var count = 0 ;
+/* para a musica */
+var audio =document.getElementById('audio');
+var playMusicBTN =document.getElementById('pause');
+var count = 0 ;
 
-    function playPause() {
-        if(count == 0) {
-            audio.play();
-            count= 1;
-        }else{
-            audio.pause();
-            count= 0;
+function playPause() {
+    if(count == 0) {
+        audio.play();
+        count= 1;
+    }else{
+        audio.pause();
+        count= 0;
 
-        }
-        
     }
     
+}
+/* PICTURE IN PICTURE */
     vid=document.getElementById("myVideo")
     vid.disablePictureInPicture = true
+/* MODAL PARA A COMANDA */
 
-    // Get the modal
-    var modal = document.getElementById("comanda");
+   // Get the modal
+   var modal    = document.getElementById("pedido");
+   var btn      = document.getElementsByClassName("far")[4];
+   var fechar   = document.getElementById("fechar");
+   var comanda  = document.getElementById("comanda");
+   var botao    = document.getElementById("myBtn");
 
-    // Get the button that opens the modal
-    var btn = document.getElementById("myBtn");
-
-    // Get the <span> element that closes the modal
-    var span = document.getElementsByClassName("close")[0];
-
-    // When the user clicks the button, open the modal 
-    btn.onclick = function() {
-        modal.style.display = "block";
-    }
-
-    // When the user clicks on <span> (x), close the modal
-    span.onclick = function() {
-    modal.style.display = "none";
-    }
-
-    // When the user clicks anywhere outside of the modal, close it
-    window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-    }
+   btn.onclick = function() {
+       modal.style.display = "block"; 
+   }
+   fechar.onclick = function() {
+       modal.style.display = "none"; 
+   }
+   botao.onclick = function() {
+       comanda.style.display = "block"; 
+   }
+   window.onclick = function(event) {
+       if (event.target == comanda) {
+           comanda.style.display = "none";
+           modal.style.display = "none";
+       }
+   }
