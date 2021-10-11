a223c1034255c2c1dad9f7a1f28d5ad52b5929fb
YuriPurcote <87339208+YuriPurcote@users.noreply.github.com>
2021-10-11 19:30:07 -0300

    add

--- JAVASCRIPT/app.js
+++ JAVASCRIPT/app.js
@@ -1,27 +1,17 @@
-window.addEventListener('scroll',()=>{
-  let subPratos = document.querySelector('#pratos');
-  let subPratosPosition = subPratos.getBoundingClientRect().top;
-  let screenPosition = window.pageYOffset;
+let subPratos = document.querySelector('#pratos');
+let subPratosPosition = subPratos.getBoundingClientRect().top;
+let screenPosition = window.pageYOffset;
+let subBebidas = document.querySelector('#bebidas');
 
+window.addEventListener('scroll',()=>{
   if(subPratosPosition < screenPosition ){
     subPratos.classList.add('Active');
+    subBebidas.classList.add('Active');
   }else{
     subPratos.classList.add('subtitulo');
     subPratos.classList.remove('Active');
-  }
-});
-
-window.addEventListener('scroll',()=>{
-  let subBebidas = document.querySelector('#bebidas');
-  let subBebidasPosition = subPratos.getBoundingClientRect().top;
-  let screenPosition = window.pageYOffset;
-
-  if(subBebidasPosition < screenPosition){
-    subBebidas.classList.add('Active');
-  }else{
     subBebidas.classList.add('subtitulo');
     subBebidas.classList.remove('Active');
   }
 });
 
-/* ANIMANDO TODOS OS PRATOS DO MENU */
